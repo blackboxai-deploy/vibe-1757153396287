ddd49c2d7f583a162c3130cb7fc6ed46b1181b32
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-06 15:40:03 +0530

    Add src/app/page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import KabaddiGame from '@/components/KabaddiGame';
+
+export default function Home() {
+  return (
+    <main className="w-full h-screen overflow-hidden">
+      <KabaddiGame />
+    </main>
+  );
+}
